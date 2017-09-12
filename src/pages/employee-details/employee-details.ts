f842455d28c4e8a39ee740613569df41822116a3
woshicheyy@sina.com <woshicheyy@sina.com>
2017-09-12 11:28:17 +0800

    增加招聘详细页面

--- src/pages/employee-details/employee-details.ts
+++ src/pages/employee-details/employee-details.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { EmployeeItem } from '../../model/EmployeeItem';
 
 /**
  * Generated class for the EmployeeDetailsPage page.
@@ -13,13 +14,27 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   selector: 'page-employee-details',
   templateUrl: 'employee-details.html',
 })
-export class EmployeeDetailsPage {
+export class EmployeeDetailsPage implements OnInit{
+
+  private item: EmployeeItem = null;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.item = navParams.data
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad EmployeeDetailsPage');
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit EmployeeDetailsPage');
+    
+  }
+
+  onApply(): void {
+    console.log('onApply EmployeeDetailsPage');
+    
+
   }
 
 }
