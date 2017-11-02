99f7fa49ce186baff9a2ef383ac4c0fe9c62295c
VitaAin <wtaincb@126.com>
2017-11-02 18:07:20 +0800

    list api

---
+++ src/api/api-resource.ts
@@ -0,0 +1,114 @@
+import { Injectable, Inject } from "@angular/core"
+import { Http, Response, Headers, RequestOptions, URLSearchParams } from "@angular/http"
+import { Storage } from "@ionic/storage";
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/toPromise";
+import { Subject, BehaviorSubject, Observable } from "rxjs";
+
+import API_ROOT from "./config";
+
+@Injectable()
+export class ApiResource {
+
+  private headers: Headers = new Headers();
+  private opts: RequestOptions = new RequestOptions();
+
+  constructor(private http: Http, private storage: Storage) {
+    this.headers.append("Content-Type", "application/json");
+    // this.interceptor();
+  }
+
+  interceptor() {
+    this.opts.headers = this.headers;
+    this.storage.get("AUTH_TOKEN").then(res => {
+      if (res && !this.opts.headers.get("Authorization")) {
+        this.opts.headers.append("Authorization", `Bearer ${res}`);
+      }
+    });
+  }
+
+  public login<T>(data): Promise<T> {
+    return this.http.post(API_ROOT + "api/Login", data, this.opts).toPromise().then(d => d.json());
+  }
+
+  public register<T>(data): Promise<T> {
+    return this.http.post(API_ROOT + "api/Register", data, this.opts).toPromise().then(d => d.json());
+  }
+
+  public updatePwd<T>(data): Promise<T> {
+    return this.http.post(API_ROOT + "ServicePerson/UpdatePwd", data, this.opts).toPromise().then(d => d.json());
+  }
+
+  public upload<T>(data): Promise<T> {
+    return this.http.post(API_ROOT + "ServicePerson/Upload", data, this.opts).toPromise().then(d => d.json());
+  }
+
+  public getPersonDetails<T>(personId, data): Promise<T> {
+    return this.http.post(API_ROOT + "ServicePerson/Details/" + personId, data, this.opts).toPromise().then(d => d.json());
+  }
+
+  public updatePersonDetails<T>(data): Promise<T> {
+    return this.http.post(API_ROOT + "ServicePerson/Update", data, this.opts).toPromise().then(d => d.json());
+  }
+
+  public getRegisterVeriCode<T>(phone, data): Promise<T> {
+    return this.http.post(API_ROOT + "api/Register/" + phone, data, this.opts).toPromise().then(d => d.json());
+  }
+
+  public getLoginVeriCode<T>(phone, data): Promise<T> {
+    return this.http.post(API_ROOT + "api/Login/" + phone, data, this.opts).toPromise().then(d => d.json());
+  }
+
+  public getEmpolyeeList<T>(areaId, data): Promise<T> {
+    return this.http.post(API_ROOT + "HotelOrder/AreaOrders/" + areaId, data, this.opts).toPromise().then(d => d.json());
+  }
+
+  public getAreaEmpolyeeList<T>(areaId, data): Promise<T> {
+    return this.http.post(API_ROOT + "api/AreaWork/" + areaId, data, this.opts).toPromise().then(d => d.json());
+  }
+
+  public getApplyRecordsList<T>(personGUID, data): Promise<T> {
+    return this.http.post(API_ROOT + "api/PersonApply/" + personGUID, data, this.opts).toPromise().then(d => d.json());
+  }
+
+  public applyWork<T>(data): Promise<T> {
+    return this.http.post(API_ROOT + "api/PersonApply", data, this.opts).toPromise().then(d => d.json());
+  }
+
+  public getHotelDetails<T>(hotelGUID, data): Promise<T> {
+    return this.http.post(API_ROOT + "Hotel/HotelDetail/" + hotelGUID, data, this.opts).toPromise().then(d => d.json());
+  }
+
+  public getAreas<T>(data): Promise<T> {
+    return this.http.post(API_ROOT + "Area/Areas", data, this.opts).toPromise().then(d => d.json());
+  }
+
+  public getMyWorkList<T>(personGUID, data): Promise<T> {
+    return this.http.post(API_ROOT + "api/PersonEmploy/" + personGUID, data, this.opts).toPromise().then(d => d.json());
+  }
+
+  public finishWork<T>(orderId, data): Promise<T> {
+    return this.http.post(API_ROOT + "api/PersonEmploy/" + orderId, data, this.opts).toPromise().then(d => d.json());
+  }
+}
+
+
+/*
+public employeeListUrl: string = URL_ROOT + "HotelOrder/AreaOrders/";//+区域id(可选)
+public areaEmployeeListUrl: string = URL_ROOT + "api/AreaWork/";//+区域id,null或0表示全部
+public applyRecordsUrl: string = URL_ROOT + "api/PersonApply/";//+用户GUID(必须)
+public applyUrl: string = URL_ROOT + "api/PersonApply";
+public hotelDetailsUrl: string = URL_ROOT + "Hotel/HotelDetail/";//+酒店GUID(必须)
+public areasInfoUrl: string = URL_ROOT + "Area/Areas";
+public myOrderUrl: string = URL_ROOT + "api/PersonEmploy/";//+用户GUID(必须)
+public finishOrderUrl: string = URL_ROOT + "api/PersonEmploy/";//+id(必须)
+
+public userLoginUrl: string = URL_ROOT + "api/Login";
+public userRegisterUrl: string = URL_ROOT + "api/Register";
+public updatePwdUrl: string = URL_ROOT + "ServicePerson/UpdatePwd";
+public userUploadUrl: string = URL_ROOT + "ServicePerson/Upload";
+public personDetailsUrl: string = URL_ROOT + "ServicePerson/Details/";//+用户Id
+public personDetailsUpdateUrl: string = URL_ROOT + "ServicePerson/Update";
+public registerVeriCodeUrl: string = URL_ROOT + "api/Register/";//+phone
+public loginVeriCodeUrl: string = URL_ROOT + "api/Login/";//+phone
+*/
---
+++ src/api/config.ts
@@ -0,0 +1,1 @@
+export const API_ROOT: string = "http://123.56.15.145:5000/";
